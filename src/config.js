b5037d98241f048320c0419c3e01b421193610d6
Tara Harwood <tara.m.harwood@gmail.com>
2021-05-04 05:38:59 -0700

    interface improvements

--- src/config.js
+++ src/config.js
@@ -53,19 +53,4 @@ const config = {
 };
 
 
- // Defines an object of custom components that will replace the stock chatbot components. 
-  // customComponents: {
-  //    // Replaces the default header
-  //   header: () => <div style={{ backgroundColor: 'red', padding: "5px", borderRadius: "3px" }}>This is the header</div>
-  //   // Replaces the default bot avatar
-  //   botAvatar: (props) => <FlightBotAvatar {...props} />,
-  //   // Replaces the default bot chat message container
-  //   botChatMessage: (props) => <CustomChatMessage {...props} />,
-  //   // Replaces the default user icon
-  //   userAvatar: (props) => <MyUserAvatar {...props} />,
-  //   // Replaces the default user chat message
-  //   userChatMessage: (props) => <MyUserChatMessage {...props} />
-  // },
-
-
 export default config;
